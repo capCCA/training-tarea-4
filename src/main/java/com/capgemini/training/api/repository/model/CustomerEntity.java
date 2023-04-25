eb40fe7701c87aacdbb80cfe40268046f54fc739
Alberto Iglesias Gonzalo <alberto.iglesias-gonzalo@capgemini.com>
2023-04-25 23:46:53 +0200

    update builder

--- src/main/java/com/capgemini/training/api/repository/model/CustomerEntity.java
+++ src/main/java/com/capgemini/training/api/repository/model/CustomerEntity.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,6 +19,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "customer")
 public class CustomerEntity extends BaseEntity {
